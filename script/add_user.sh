f756b167a62217806f51410d31e62ab4015914af
hyliang96 <hyliang96@hotmail.com>
2019-04-15 18:11:37 +0800

    logout push sshost5 2019-04-15 18:11:37

--- script/add_user.sh
+++ script/add_user.sh
@@ -11,3 +11,10 @@ password=$4
 useradd "$username" -m -c "$realname" -s /usr/bin/zsh -u $uid
 echo "$username:$password" | chpasswd
 # }
+
+
+# 一键多个服务器开用户
+# ssh c3
+# run `openssl rand -base64 24` to get 密码
+# ANSIBLE_SSH_ARGS="-F ~/.ssh/config_JUN1" ansible jungpuall -m user -a 'name=用户名 uid=用户的UID值需要统一制定 password=密码' --sudo --ask-sudo-pass
+# jungpuall 指gpu1-gpu24，可以换成其他的，详见/etc/ansible/hosts
